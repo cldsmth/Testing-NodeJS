99e0247952a898aa350914111b4dba03483c0b39
Cliff Dean Smith <dean11.cliff@gmail.com>
2019-09-07 12:47:17 +0700

    request multiple promise

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -3,11 +3,17 @@ var request = require('request');
 
 exports.get_detail = function(req, res){
 	try{
-		var adminProfile = getProfile();
-		adminProfile.then(resolve => {
-			var adminDetails = resolve;
-			res.status(200).send({status: 200, message: 'Data exist', data: adminDetails});
-		}).catch(reject => {
+        var adminProfileURL = 'https://api.github.com/users/narenaryan';
+        var adminProfile = getProfile(adminProfileURL);
+        //Get user details after that get followers from URL
+        adminProfile.then(resolve => {
+            var adminDetails = resolve;
+            //Do one more async operation here
+            var myFollowers = getProfile(adminDetails.followers_url);
+            return myFollowers;
+        }).then(result => {
+            res.status(200).send({status: 200, message: 'Data exist', data: result});
+        }).catch(reject => {
 			res.status(400).send({status: 400, message: reject.toString()});
 		});
 	}catch(error){
@@ -15,17 +21,17 @@ exports.get_detail = function(req, res){
 	}
 };
 
-function getProfile(){
-	//setting URL and headers for request
+function getProfile(url){
+	//Setting URL and headers for request
     var options = {
-        url: 'https://api.github.com/users/narenaryan',
+        url: url,
         headers: {
             'User-Agent': 'request'
         }
     };
-    //return new promise 
+    //Return new promise 
     return new Promise(function(resolve, reject){
-    	//do async job
+    	//Do async job
         request.get(options, function(err, resp, body){
             if(err){
             	reject(err);
