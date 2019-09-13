3f1c744f2828de1d82460274abdd31dc057b770a
Cliff Dean Smith <dean11.cliff@gmail.com>
2019-09-13 23:45:18 +0700

    crud product

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -15,3 +15,64 @@ exports.get_all = function(req, res) {
         functions.BaseResponse(res, 400, error);
     }
 };
+
+exports.get_detail = function(req, res) {
+    try{
+        conn.query('SELECT * FROM product WHERE product_id = ?', [req.params.id], function(err, rows, fields) {
+            if(err){
+                functions.ArrayResponse(res, 400, "Error", err);
+            }else{
+                functions.ArrayResponse(res, 200, "Data exist", rows);
+            }
+        });
+    }catch(error){
+        functions.BaseResponse(res, 400, error);
+    }
+};
+
+exports.insert_data = function(req, res) {
+    try{
+        var name = req.body.name;
+        var price = req.body.price;
+        conn.query('INSERT INTO product (product_name, product_price) values (?, ?)', [name, price], function(err, rows, fields) {
+            if(err){
+                functions.ArrayResponse(res, 400, "Error", err);
+            } else{
+                functions.BaseResponse(res, 200, "Insert success");
+            }
+        });
+    }catch(error){
+        functions.BaseResponse(res, 400, error);
+    }
+};
+
+exports.update_data = function(req, res) {
+    try{
+        var id = req.body.id;
+        var name = req.body.name;
+        var price = req.body.price;
+        conn.query('UPDATE product SET product_name = ?, product_price = ? WHERE product_id = ?', [name, price, id], function(err, rows, fields) {
+            if(err){
+                functions.ArrayResponse(res, 400, "Error", err);
+            } else{
+                functions.BaseResponse(res, 200, "Update success");
+            }
+        });
+    }catch(error){
+        functions.BaseResponse(res, 400, error);
+    }
+};
+
+exports.delete_data = function(req, res) {
+    try{
+        conn.query('DELETE FROM product WHERE product_id = ?', [req.params.id], function(err, rows, fields) {
+            if(err){
+                functions.ArrayResponse(res, 400, "Error", err);
+            }else{
+                functions.BaseResponse(res, 200, "Delete success");
+            }
+        });
+    }catch(error){
+        functions.BaseResponse(res, 400, error);
+    }
+};
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -3,5 +3,9 @@ var router = require('express').Router(),
 	product = require('../controllers/productController');
 
 router.get('/list', product.get_all);
+router.get('/:id', product.get_detail);
+router.post('/', product.insert_data);
+router.put('/', product.update_data);
+router.delete('/:id', product.delete_data);
  
 module.exports = router;
