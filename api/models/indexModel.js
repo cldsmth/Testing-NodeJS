c62197690b043c5463e4cc05638b146aa2eb17c8
Cliff Dean Smith <dean11.cliff@gmail.com>
2019-09-17 00:29:57 +0700

    knex select specific

--- api/models/indexModel.js
+++ api/models/indexModel.js
@@ -4,9 +4,31 @@ const exec = require('../../helpers/connection'),
 
 module.exports = {
     getAll: function(req, callback){
-        return exec.knex(table)
-            .select('*')
-            .orderBy('created_at', 'DESC')
+        var column = [
+            'product.id', 
+            'product.name', 
+            'product.price', 
+            exec.knex.raw(`CASE WHEN product.status = 1
+                THEN 'Publish'
+                WHEN product.status = '2'
+                THEN 'Deleted'
+                ELSE 'Not Publish'
+                END as status
+            `),
+            'product.created_at',
+            'category.id as category_id', 
+            'category.name as category_name', 
+            'color.id as color_id', 
+            'color.name as color_name'
+        ]
+        return exec.knex(table + ' as product')
+            .select(column)
+            .leftJoin('category as category', 'category.id', '=', 'product.category_id')
+            .leftJoin('color as color', 'color.id', '=', 'product.color_id')
+            .where({status: 1})
+            .andWhere('product.price', '>', 3000)
+            .orderBy('product.created_at', 'desc')
+            .limit(5)
             .then(datas => {
                 callback(null, datas)
             }).catch(function(error){ 
